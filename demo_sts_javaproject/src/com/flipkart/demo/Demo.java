2f12e7cfcd6b576ba77607ceda0d990782abbfc2
sujithareddy23 <reddysujitha23@gmail.com>
2026-01-22 16:20:51 +0530

    Second commit

---
+++ demo_sts_javaproject/src/com/flipkart/demo/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Second project by sts");
+	}
+
+}
---
+++ demo_sts_javaproject/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module demo_sts_javaproject {
+}
